9c38821d33287f3ae84b181e29d76489a5e61d3e
NuV00 <128266226+NuV00@users.noreply.github.com>
2023-04-15 23:52:13 +0700

    Update NhapXuat.java

--- NhapXuat.java
+++ NhapXuat.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package TX1;
 
-/**
- *
- * @author ngoct
- */
 public interface NhapXuat {
     final int TIEN_THUE_BAO = 3000;
     final int MUC_TRAN = 400000;
